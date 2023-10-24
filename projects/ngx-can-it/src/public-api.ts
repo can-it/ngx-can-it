53a8016b9c9a44cb85e51605acdbc0557c7f6941
Hai Le <haile.vnm@gmail.com>
2023-10-24 17:44:14 +0700

    feat: export models

--- projects/ngx-can-it/src/public-api.ts
+++ projects/ngx-can-it/src/public-api.ts
@@ -5,5 +5,9 @@
 export * from './lib/can-it.module';
 
 export * from './lib/directives/can-it.directive';
+
 export * from './lib/services/can-it.service';
 export * from './lib/services/permissions-store.service';
+
+export * from './lib/types/permission';
+export * from './lib/types/permissions-state';
