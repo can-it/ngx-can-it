cbf3e4c752995c3407868472f1c067c1421876aa
Hai Le <haile.vnm@gmail.com>
2023-10-24 10:54:51 +0700

    chore: add ngx-can-it project and change npm to pnpm

---
+++ projects/ngx-can-it/src/lib/ngx-can-it.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { NgxCanItComponent } from './ngx-can-it.component';
+
+
+
+@NgModule({
+  declarations: [
+    NgxCanItComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    NgxCanItComponent
+  ]
+})
+export class NgxCanItModule { }
---
+++ projects/ngx-can-it/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of ngx-can-it
+ */
+
+export * from './lib/ngx-can-it.service';
+export * from './lib/ngx-can-it.component';
+export * from './lib/ngx-can-it.module';
