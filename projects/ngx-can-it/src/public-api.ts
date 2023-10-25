96365c2ad38ee0817b95c789cfe1d328cf5dbeaf
Hai Le <haile.vnm@gmail.com>
2023-10-25 17:31:01 +0700

    fix: public canIt pipe

--- projects/ngx-can-it/src/public-api.ts
+++ projects/ngx-can-it/src/public-api.ts
@@ -5,6 +5,7 @@
 export * from './lib/ngx-can-it.module';
 
 export * from './lib/directives/can-it.directive';
+export * from './lib/pipes/can-it.pipe';
 
 export * from './lib/services/can-it.service';
 export * from './lib/services/permissions-store.service';
