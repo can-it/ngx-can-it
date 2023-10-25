a0803987078b9edb9f4fcd2692f2e27eaa95796a
Hai Le <haile.vnm@gmail.com>
2023-10-25 11:33:12 +0700

    test: refactor the canit pipe

--- projects/ngx-can-it/src/lib/pipes/can-it.pipe.spec.ts
+++ projects/ngx-can-it/src/lib/pipes/can-it.pipe.spec.ts
@@ -4,6 +4,7 @@ import { CanItService } from '../services/can-it.service';
 import { Request } from '../types/permission';
 import { ReplaySubject } from 'rxjs';
 import { Component } from '@angular/core';
+import { expectContent } from '../../spec-helpers/element';
 
 @Component({
   template: '{{request | canIt | async}}',
@@ -29,7 +30,7 @@ describe('CanItPipe: with TestBed and HostComponent', () => {
     await TestBed.configureTestingModule({
       declarations: [CanItPipe, HostComponent],
       providers: [
-        { provide: CanItService, useValue: canItService }
+        { provide: CanItService, useValue: canItService },
       ],
     }).compileComponents();
 
@@ -41,22 +42,22 @@ describe('CanItPipe: with TestBed and HostComponent', () => {
   it('should display "true" when the observable value returned by canItService is true', () => {
     canSubject$.next(true)
     fixture.detectChanges();
-    expect(fixture.nativeElement.textContent).toEqual('true');
+    expectContent(fixture).toEqual('true');
   });
   
   it('should display "false" when the observable value returned by canItService is false', () => {
     canSubject$.next(false);
     fixture.detectChanges();
-    expect(fixture.nativeElement.textContent).toEqual('false');
+    expectContent(fixture).toEqual('false');
   });
 
   it('should update the new value when the can method emits a new value', () => {
     canSubject$.next(true);
     fixture.detectChanges();
-    expect(fixture.nativeElement.textContent).toEqual('true');
+    expectContent(fixture).toEqual('true');
 
     canSubject$.next(false);
     fixture.detectChanges();
-    expect(fixture.nativeElement.textContent).toEqual('false');
+    expectContent(fixture).toEqual('false');
   });
 });
